df05cd9d611317ee3156f818a73ff79d9c2dc785
leersmathieu <leers.mathieu@gmail.com>
2018-02-20 16:41:57 +0100

    trying JSON

--- formulaire.php
+++ formulaire.php
@@ -1,12 +1,38 @@
 <?php
 
 $jsonURL="todo.json";
+
 $jsonReceived = file_get_contents($jsonURL);
-$datafile =  [$date,$childName, $teacherName, $reason];
-$log = json_decode($dataReceived) ;
-$log[] = $datafile;
-$justifResult = json_encode($log);
-file_put_contents($jsonURL, $justifResult);
+
+$log = json_decode($jsonReceived) ;
+
+if (isset($_POST['ajouter'])){
+
+$add_tache = $_POST['tache'];
+
+
+
+$array_tache = array("nomtache" => $add_tache,
+                    "fin" => false);
+
+$log[] = $array_tache;
+
+
+$json_enc= json_encode($log, JSON_PRETTY_PRINT);
+
+var_dump($json_enc);
+file_put_contents($jsonURL, $json_enc);
+
+}
+
+// if (isset($_POST['boutton'])){
+
+
+// }
+
+
+
+// var_dump($add_tache);
 
 ?>
 
@@ -22,14 +48,29 @@ file_put_contents($jsonURL, $justifResult);
 <body>
     <div class="page">
         <section class="afaire">
-            <form action="" method="post" name="formafaire">
-                <input type="checkbox" name="choix" value="" /><label for="choix">Course</label>
+            <form action="formulaire.php" method="post" name="formafaire">
+
+                <?php
+
+                    $i = 0;
+                    foreach ($log as $key => $value){
+                       
+                       if ($value->{"fin"} == false)
+                        echo "<input type='checkbox' name='choix' value=".$i."/><label for='choix'>".$value->{"nomtache"}."</label><br />";
+                        $i++;
+                    }
+                ?>
+                <input type="submit" nom="boutton" value="Check" >
                 
             </form>
         </section>
 
         <section class="archive">
+                <?php
 
+                    
+                    
+                ?>
         </section>
 
         <footer class="tache">
@@ -39,6 +80,7 @@ file_put_contents($jsonURL, $justifResult);
               <label for="tache">La tâche à effectuer</label>
               <input type="text" name="tache" value="">
               <input type="submit" name="ajouter" value="Ajouter">
+              <pre><?php print_r($_POST); ?></pre>
             </form>
           </fieldset>
         </footer>
